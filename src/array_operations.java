c41bfacc96f6db133e91651c876f3e665737d316
Ashmaawyy <al_ashmawy@outlook.com>
2022-05-22 18:46:41 +0200

    The code is beautiful, but it won't run :'(

--- src/array_operations.java
+++ src/array_operations.java
@@ -1,11 +1,13 @@
 import java.util.*;
 
-public class array_operations {
-    public static void main(String[] args) {
+public class array_operations
+{
+    public static void main(String[] args)
+    {
         int[] array = {5, 6, 7, 8, 8, 20, 12, 15, 5, 6, 7, 8};
         System.out.println("AVERAGE of array " + Arrays.toString(array) + " is " + find_average(array));
         System.out.println("Frequency of elements in array " + " is: " + find_frequency(array));
-}
+    }
 private static Hashtable<Integer, Integer> find_frequency(int [] input_array)
 {
     Hashtable<Integer, Integer> frequency_dict = new Hashtable <Integer, Integer> ();
@@ -17,7 +19,8 @@ private static Hashtable<Integer, Integer> find_frequency(int [] input_array)
 
     return frequency_dict;
 }
-    private static double find_average(int[] values) {
+    private static double find_average(int[] values)
+    {
         double result = 0;
         for (int value : values) {
             result += value;
