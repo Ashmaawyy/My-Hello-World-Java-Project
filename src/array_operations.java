e52783f01c7a80a1c93ce2ead460be4a8ff12aa7
Ashmaawyy <al_ashmawy@outlook.com>
2022-05-22 18:09:09 +0200

    Trying to calculate frequency of objects in Java : )

--- src/array_operations.java
+++ src/array_operations.java
@@ -1,15 +1,31 @@
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class array_operations {
     public static void main(String[] args) {
-        int[] array = {5, 6, 7, 8};
-        System.out.println("AVERAGE of array " + Arrays.toString(array) + " is " + findAverage(array));
+        int[] array = {5, 6, 7, 8, 8, 20, 12, 15, 5, 6, 7, 8};
+        System.out.println("AVERAGE of array " + Arrays.toString(array) + " is " + find_average(array));
+        System.out.println("Frequency of elements in array " + " is: " + find_frequency(array));
 }
-    private static double findAverage(int[] values) {
+private static Hashtable<Integer, Integer> find_frequency(int [] array)
+{
+    Hashtable<Integer, Integer> frequency_dict = new Hashtable <Integer, Integer> ();
+    List<Integer> frequencies = new ArrayList<>();
+    for (int j : array)
+    {
+        frequencies.add(Collections.frequency(List.of(array), j));
+        System.out.println(frequencies);
+    }
+    for (int i = 0; i < array.length; i++)
+    {
+        frequency_dict.put(array[i], i);
+    }
+
+    return frequency_dict;
+}
+    private static double find_average(int[] values) {
         double result = 0;
-        for (int i = 0; i < values.length; i++) {
-            result += values[i];
+        for (int value : values) {
+            result += value;
         }
         return result / values.length;
     }
