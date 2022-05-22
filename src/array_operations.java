a5b3a4719c60ec819ae6a8d937ab6525bc124026
Ashmaawyy <al_ashmawy@outlook.com>
2022-05-22 18:44:47 +0200

    The code is beautiful, but it won't run :'(

--- src/array_operations.java
+++ src/array_operations.java
@@ -6,18 +6,13 @@ public class array_operations {
         System.out.println("AVERAGE of array " + Arrays.toString(array) + " is " + find_average(array));
         System.out.println("Frequency of elements in array " + " is: " + find_frequency(array));
 }
-private static Hashtable<Integer, Integer> find_frequency(int [] array)
+private static Hashtable<Integer, Integer> find_frequency(int [] input_array)
 {
     Hashtable<Integer, Integer> frequency_dict = new Hashtable <Integer, Integer> ();
-    List<Integer> frequencies = new ArrayList<>();
-    for (int j : array)
-    {
-        frequencies.add(Collections.frequency(List.of(array), j));
-        System.out.println(frequencies);
-    }
-    for (int i = 0; i < array.length; i++)
-    {
-        frequency_dict.put(array[i], i);
+    Set<Integer> frequencies = new HashSet<>();
+
+    for (int j : input_array) {
+        frequency_dict.put(j, Collections.frequency(List.of(input_array), j));
     }
 
     return frequency_dict;
