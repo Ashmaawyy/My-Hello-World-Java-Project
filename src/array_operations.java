734e1a52144eed88eff9b77fc74aad7041ce973a
Ashmaawyy <al_ashmawy@outlook.com>
2022-05-23 21:20:26 +0200

    At last Frequency is working and saved into a Hashtable (i.e Dictionary) :')

--- src/array_operations.java
+++ src/array_operations.java
@@ -8,8 +8,7 @@ public class array_operations
     {
         int[] array = {5, 6, 7, 8, 8, 20, 12, 15, 5, 6, 7, 8};
         System.out.println("AVERAGE of array is: " + find_average(array));
-        find_frequency(array);
-        //System.out.println("Frequency of elements in array " + " is: " + find_frequency(array));
+        System.out.println("Frequency of elements in array " + " is: " + find_frequency(array));
         System.out.println("Sorted Array From A-Z: " + Arrays.toString(get_sorted_array(array, "asc")));
         System.out.println("Sorted Array From Z-A: " + Arrays.toString(get_sorted_array(array, "dec")));
     }
@@ -45,13 +44,10 @@ public class array_operations
             }
             return input_array;
     }
-    private static void find_frequency(int [] input_array)
-{
-    Hashtable<Integer, Integer> frequency_dict = new Hashtable<Integer, Integer> ();
-
-    int [] frequencies_array = new int[input_array.length];
-    int visited = -1;
-    //Arrays.fill(frequencies_array, 1);
+    private static Hashtable<Integer, Integer> find_frequency(int [] input_array)
+    {
+        int [] frequencies_array = new int[input_array.length];
+        int visited = -1;
         for(int i = 0; i < input_array.length; i++)
         {
             int count = 1;
@@ -66,15 +62,14 @@ public class array_operations
 
             }
         }
-    Formatter fmt = new Formatter();
-        fmt.format("%15s%15s\n", "Elements", "Frequency");
-        for (int i = 0; i < frequencies_array.length; i++)
+        Hashtable <Integer, Integer> frequency_dict = new Hashtable<Integer, Integer>();
+        for (int i = 0; i < input_array.length; i++)
         {
             if (frequencies_array[i] != visited)
-                fmt.format("%14s%14s\n", input_array[i], frequencies_array[i]);
+                frequency_dict.put(input_array[i], frequencies_array[i]);
         }
-        System.out.println(fmt);
-}
+        return frequency_dict;
+    }
     private static double find_average(int[] values)
     {
         double result = 0;
