7fb72445b555816572f4afe306ceb681275f8eff
Ashmaawyy <al_ashmawy@outlook.com>
2022-05-23 18:26:15 +0200

    At last Frequency is working :')

--- src/array_operations.java
+++ src/array_operations.java
@@ -8,7 +8,8 @@ public class array_operations
     {
         int[] array = {5, 6, 7, 8, 8, 20, 12, 15, 5, 6, 7, 8};
         System.out.println("AVERAGE of array is: " + find_average(array));
-        System.out.println("Frequency of elements in array " + " is: " + find_frequency(array));
+        find_frequency(array);
+        //System.out.println("Frequency of elements in array " + " is: " + find_frequency(array));
         System.out.println("Sorted Array From A-Z: " + Arrays.toString(get_sorted_array(array, "asc")));
         System.out.println("Sorted Array From Z-A: " + Arrays.toString(get_sorted_array(array, "dec")));
     }
@@ -44,36 +45,35 @@ public class array_operations
             }
             return input_array;
     }
-    private static Hashtable<Integer, Integer> find_frequency(int [] input_array)
+    private static void find_frequency(int [] input_array)
 {
     Hashtable<Integer, Integer> frequency_dict = new Hashtable<Integer, Integer> ();
 
     int [] frequencies_array = new int[input_array.length];
-    int count = 1;
-    //System.arraycopy(input_array, 0, frequencies_array, 0, input_array.length);
-        for(int j = 0; j < input_array.length; j++)
+    int visited = -1;
+    //Arrays.fill(frequencies_array, 1);
+        for(int i = 0; i < input_array.length; i++)
         {
-            for (int i = 0; i < input_array.length; i++)
-            {
-                if (input_array[i] == frequencies_array[i])
-                {
+            int count = 1;
+            for (int j = i + 1; j < input_array.length; j++) {
+                if (input_array[i] == input_array[j]) {
                     count++;
+                    frequencies_array[j] = visited;
+                }
+                if (frequencies_array[i] != visited) {
                     frequencies_array[i] = count;
                 }
-                else
-                    frequencies_array[i] = 1;
+
             }
         }
-    System.out.println("Elements: " + Arrays.toString(input_array) + "\n" + "Frequency: " + Arrays.toString(frequencies_array));
-    for (int j : input_array)
-    {
-        for (int frequency : frequencies_array)
+    Formatter fmt = new Formatter();
+        fmt.format("%15s%15s\n", "Elements", "Frequency");
+        for (int i = 0; i < frequencies_array.length; i++)
         {
-            frequency_dict.put(j, frequency);
+            if (frequencies_array[i] != visited)
+                fmt.format("%14s%14s\n", input_array[i], frequencies_array[i]);
         }
-    }
-
-    return frequency_dict;
+        System.out.println(fmt);
 }
     private static double find_average(int[] values)
     {
