228ee41ec12b414483d81c2a42618bd3c6fe7601
Ashmaawyy <al_ashmawy@outlook.com>
2022-05-23 17:13:29 +0200

    Trying to solve frequency calculation bug : )

--- src/array_operations.java
+++ src/array_operations.java
@@ -1,27 +1,33 @@
 import java.util.*;
 
+import static java.lang.Math.abs;
+
 public class array_operations
 {
     public static void main(String[] args)
     {
         int[] array = {5, 6, 7, 8, 8, 20, 12, 15, 5, 6, 7, 8};
-        System.out.println("AVERAGE of array " + Arrays.toString(array) + " is " + find_average(array));
+        System.out.println("AVERAGE of array is: " + find_average(array));
         System.out.println("Frequency of elements in array " + " is: " + find_frequency(array));
         System.out.println("Sorted Array From A-Z: " + Arrays.toString(get_sorted_array(array, "asc")));
         System.out.println("Sorted Array From Z-A: " + Arrays.toString(get_sorted_array(array, "dec")));
     }
 
     private static int[] get_sorted_array(int[] input_array, String option)
-        {
+    {
             if (Objects.equals(option, "ascending") || Objects.equals(option, "asc"))
             {
                 Arrays.sort(input_array);
             }
-            else if (Objects.equals(option, "dec") || Objects.equals(option, "descending")) {
+            else if (Objects.equals(option, "dec") || Objects.equals(option, "descending"))
+            {
                 int temp;
-                for (int i = 0; i < input_array.length; i++) {
-                    for (int j = i + 1; j < input_array.length; j++) {
-                        if (input_array[i] < input_array[j]) {
+                for (int i = 0; i < input_array.length; i++)
+                {
+                    for (int j = i + 1; j < input_array.length; j++)
+                    {
+                        if (input_array[i] < input_array[j])
+                        {
                             temp = input_array[i];
                             input_array[i] = input_array[j];
                             input_array[j] = temp;
@@ -37,13 +43,34 @@ public class array_operations
                         'dec' or 'asc'\s""");
             }
             return input_array;
-        }
+    }
     private static Hashtable<Integer, Integer> find_frequency(int [] input_array)
 {
     Hashtable<Integer, Integer> frequency_dict = new Hashtable<Integer, Integer> ();
 
-    for (int j : input_array) {
-        frequency_dict.put(j, Collections.frequency(List.of(input_array), j));
+    int [] frequencies_array = new int[input_array.length];
+    int count = 1;
+    //System.arraycopy(input_array, 0, frequencies_array, 0, input_array.length);
+        for(int j = 0; j < input_array.length; j++)
+        {
+            for (int i = 0; i < input_array.length; i++)
+            {
+                if (input_array[i] == frequencies_array[i])
+                {
+                    count++;
+                    frequencies_array[i] = count;
+                }
+                else
+                    frequencies_array[i] = 1;
+            }
+        }
+    System.out.println("Elements: " + Arrays.toString(input_array) + "\n" + "Frequency: " + Arrays.toString(frequencies_array));
+    for (int j : input_array)
+    {
+        for (int frequency : frequencies_array)
+        {
+            frequency_dict.put(j, frequency);
+        }
     }
 
     return frequency_dict;
@@ -51,7 +78,8 @@ public class array_operations
     private static double find_average(int[] values)
     {
         double result = 0;
-        for (int value : values) {
+        for (int value : values)
+        {
             result += value;
         }
         return result / values.length;
