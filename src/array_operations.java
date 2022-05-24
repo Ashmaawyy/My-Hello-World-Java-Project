af9e1a053a942ca8c5a0273a40186d0a574f9553
Ashmaawyy <al_ashmawy@outlook.com>
2022-05-24 04:25:07 +0200

    The code is now more concise and clean : )

--- src/array_operations.java
+++ src/array_operations.java
@@ -8,7 +8,7 @@ public class array_operations
     {
         int[] array = {5, 6, 7, 8, 8, 20, 12, 15, 5, 6, 7, 8, 20, 12, 15};
         System.out.println("AVERAGE of array is: " + find_average(array));
-        System.out.println("Frequency of elements in array " + " is: " + find_frequency(array));
+        System.out.println("Frequency of elements in array " + " is: " + get_items_frequency(array));
         System.out.println("Sorted Array From A-Z: " + Arrays.toString(get_sorted_array(array, "asc")));
         System.out.println("Sorted Array From Z-A: " + Arrays.toString(get_sorted_array(array, "dec")));
     }
@@ -44,10 +44,26 @@ public class array_operations
             }
             return input_array;
     }
-    private static Hashtable<Integer, Integer> find_frequency(int [] input_array)
+    private static Hashtable<Integer, Integer> get_items_frequency(int [] input_array)
     {
-        int [] frequencies_array = new int[input_array.length];
         get_sorted_array(input_array, "asc");
+        int[] frequency_array = get_frequency_array(input_array);
+        return get_frequency_dict(input_array, frequency_array);
+    }
+    private static Hashtable <Integer, Integer> get_frequency_dict(int[] input_array, int[] frequency_array)
+    {
+        Hashtable <Integer, Integer> frequency_dict = new Hashtable<Integer, Integer>();
+        int visited = -1;
+        for (int i = 0; i < input_array.length; i++)
+        {
+            if (frequency_array[i] != visited)
+                frequency_dict.put(input_array[i], frequency_array[i]);
+        }
+        return frequency_dict;
+    }
+    private static int [] get_frequency_array(int[] input_array)
+    {
+        int [] frequency_array = new int[input_array.length];
         int visited = -1;
         for(int i = 0; i < input_array.length; i++)
         {
@@ -57,22 +73,14 @@ public class array_operations
                 if (input_array[i] == input_array[j])
                 {
                     count++;
-                    frequencies_array[j] = visited;
+                    frequency_array[j] = visited;
                 }
-                if (frequencies_array[i] != visited)
-                {
-                    frequencies_array[i] = count;
-                }
+                if (frequency_array[i] != visited)
+                    frequency_array[i] = count;
 
             }
         }
-        Hashtable <Integer, Integer> frequency_dict = new Hashtable<Integer, Integer>();
-        for (int i = 0; i < input_array.length; i++)
-        {
-            if (frequencies_array[i] != visited)
-                frequency_dict.put(input_array[i], frequencies_array[i]);
-        }
-        return frequency_dict;
+        return frequency_array;
     }
     private static double find_average(int[] values)
     {
